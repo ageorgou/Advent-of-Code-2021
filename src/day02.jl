a2a1f28f354b57d74044a6f0b9f97320ce8a1a8b
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2021-12-02 18:01:06 +0000

    Add first pass for Day 2 Part 2 (works)

--- src/day02.jl
+++ src/day02.jl
@@ -11,21 +11,40 @@ forward 2"
 
 
 function move(initial, instruction::String)
-    horizontal, depth = initial
     parts = split(instruction)
     if length(parts) != 2
         error("Malformed instruction $instruction")
     end
     command, number = parts[1], parse(Int, parts[2])
     if command == "forward"
-        (horizontal + number, depth)
+        displacement = (number, 0)
     elseif command == "down"
-        (horizontal, depth + number)
+        displacement = (0, number)
     elseif command == "up"
-        (horizontal, depth - number)
+        displacement = (0, -number)
     else
         error("Unrecognised command $command")
     end
+    initial .+ displacement
+end
+
+function moveWithAim(initial, instruction::String)
+    aim = initial[3]
+    parts = split(instruction)
+    if length(parts) != 2
+        error("Malformed instruction $instruction")
+    end
+    command, number = parts[1], parse(Int, parts[2])
+    if command == "forward"
+        displacement = (number, number * aim, 0)
+    elseif command == "down"
+        displacement = (0, 0, number)
+    elseif command == "up"
+        displacement = (0, 0, -number)
+    else
+        error("Unrecognised command $command")
+    end
+    initial .+ displacement
 end
 
 function solve(io::IO)
@@ -36,8 +55,17 @@ function solve(io::IO)
     position, prod(position)
 end
 
+function solvePart2(io::IO)
+    position = (0, 0, 0)
+    for line in eachline(io)
+        position = moveWithAim(position, line)
+    end
+    position[1:2], prod(position[1:2])
+end
+
 @testset "day02" begin
     @test solve(IOBuffer(TEST_STRING)) == ((15, 10), 150)
+    @test solvePart2(IOBuffer(TEST_STRING)) == ((15, 60), 900)
 end
 
 end #module
