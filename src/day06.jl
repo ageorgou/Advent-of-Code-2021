e82050817468df40457ac3551720903507cd115f
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2021-12-06 11:50:32 +0000

    Update for day 6 part 2

--- src/day06.jl
+++ src/day06.jl
@@ -6,23 +6,30 @@ function nextDay(n::Int)
     n > 0 ? (n - 1) : 6
 end
 
-function updateAges(ages::Vector{Int})
-    # NB: iterates over the list twice
-    nReadyToSpawn = sum(ages .== 0)
-    vcat([nextDay(age) for age in ages], 8 * ones(Int, nReadyToSpawn))
+function updateAges(counts::Dict{Int, Int})
+    newCounts = Dict{Int, Int}(n => 0 for n in range(0, 8, step=1))
+    for (k, v) in counts
+        newCounts[nextDay(k)] += v
+    end
+    newCounts[8] = counts[0]
+    return newCounts
 end
 
-function simulate(init::Vector{Int}, nDays::Int)
-    ages = init
+function simulate(init::Dict{Int, Int}, nDays::Int)
+    counts = init
     for _ in range(1, length=nDays)
-        ages = updateAges(ages)
+        counts = updateAges(counts)
     end
-    ages
+    counts
 end
 
 function solve(io::IO; nDays=80)
-    init = [parse(Int, s) for s in split(readline(io), ',')]
-    length(simulate(init, nDays))
+    ages = [parse(Int, s) for s in split(readline(io), ',')]
+    init = Dict{Int, Int}(n => 0 for n in range(0, 8, step=1))
+    for age in ages
+        init[age] += 1
+    end
+    sum(values(simulate(init, nDays)))
 end
 
 const TEST_INPUT = "3, 4, 3, 1, 2"
@@ -30,6 +37,7 @@ const TEST_INPUT = "3, 4, 3, 1, 2"
 @testset "final" begin
     @test solve(IOBuffer(TEST_INPUT), nDays=18) == 26
     @test solve(IOBuffer(TEST_INPUT), nDays=80) == 5934
+    @test solve(IOBuffer(TEST_INPUT), nDays=256) == 26984457539
 end
 
 end
