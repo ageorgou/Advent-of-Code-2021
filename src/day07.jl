dfe94e989d9fa4d873fcc3e58ae2e9035d08fc4e
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2021-12-07 19:09:08 +0000

    Day 7 part 2 (works)

--- src/day07.jl
+++ src/day07.jl
@@ -15,15 +15,39 @@ function optimalCost(locations)
     minimum(sum(dists, dims=1))
 end
 
-function solve(io::IO)
-    locs = [parse(Int, s) for s in split(readline(io), ',')]
-    optimalCost(locs)
+function compoundCost(distance)::Int
+    (distance * (distance + 1)) / 2
 end
+
+function compoundCost(locations, target)::Int
+    dists = abs.(locations .- target)
+    sum(compoundCost.(dists))
+end
+
+function optimalCompoundCost(locations)
+    # Not true! Optimal solution may not match any initial position
+    # dists = abs.(distances(locations))
+    # minimum(sum(compoundCost.(dists), dims=1))
+    minimum(
+        compoundCost(locations, x)
+        for x in minimum(locations):maximum(locations)
+    )
+end
+
+function solve(io::IO, compound=false)
+    locs = [parse(Int, s) for s in split(readline(io), ',')]
+    compound ? optimalCompoundCost(locs) : optimalCost(locs)
+end
+
 
 TEST_INPUT = [16,1,2,0,4,2,7,1,2,14]
 
 @testset "day07" begin
     @test optimalCost(TEST_INPUT) == 37
+    @test compoundCost(3) == 6
+    @test compoundCost(TEST_INPUT, 5) == 168
+    @test compoundCost(TEST_INPUT, 2) == 206
+    @test optimalCompoundCost(TEST_INPUT) == 168
 end
 
 end
